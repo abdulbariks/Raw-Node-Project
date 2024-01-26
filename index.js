9a18bc2f5b94d86c0a2b6f0eaa1f9205a156af54
Abdul Barik <abdulbarik1997m@gmail.com>
2024-01-27 02:09:36 +0600

    Create Node  Server

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// dependencies
+const http = require('http');
+
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {
+    port: 3000,
+};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`listening to port ${app.config.port}`);
+    });
+};
+
+// handle Request Response
+app.handleReqRes = (req, res) =>{
+       res.end("hello node js");
+};
+
+// start the server
+app.createServer();
